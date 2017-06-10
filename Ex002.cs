495fa430417d46c999472a0c324871ef6051ebd6
Kyungwook Cho <amisay5256@naver.com>
2017-06-10 22:28:08 +0900

    정수형 기본 타입

---
+++ Ex002.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Ex002
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            System.Int32 n1 = 50;
+            System.Int32 n2;
+            n2 = 100;
+
+            System.Int32 sum = n1 + n2;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
