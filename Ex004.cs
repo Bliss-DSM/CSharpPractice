00323cb3296a132f3177751c9c2fac275d8418f0
Kyungwook Cho <amisay5256@naver.com>
2017-06-10 22:28:47 +0900

    문자형/불린형 기본 타입

---
+++ Ex004.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Ex004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch1 = 'A';
+            Console.WriteLine(ch1);
+
+            char ch2 = '\u2023';
+            Console.WriteLine(ch2);
+
+            string text1 = "Hello World";
+            Console.WriteLine(text1);
+
+            string text2 = "\tHello\nWorld";
+            Console.WriteLine(text2);
+
+            string text3 = @"\tHello\nWorld";
+            Console.WriteLine(text3);
+
+            bool isNumeric = false;
+            Console.WriteLine(isNumeric);
+        }
+    }
+}
