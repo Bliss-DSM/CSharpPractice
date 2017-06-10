285afdede90d3b8b1fb5b6266e5ef4761ff98ca5
Kyungwook Cho <amisay5256@naver.com>
2017-06-10 22:21:49 +0900

    기본 자료형

---
+++ Ex001.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Ex001
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n1 = 50;
+            int n2;
+            n2 = 100;
+
+            long sum = n1 + n2;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
